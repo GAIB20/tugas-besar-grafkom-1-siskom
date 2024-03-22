d2a8c042347786757726a6a9e8e2f864d46dd4e0
Rinaldy Adin <16521390@mahasiswa.itb.ac.id>
2024-03-22 17:37:01 +0700

    feat: choose shape & make shape from clicks

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import AppCanvas from "./AppCanvas";
-import Color from "./Base/Color";
-import Line from "./Shapes/Line";
-import Triangle from "./Shapes/Triangle";
-import init from "./init";
+import AppCanvas from './AppCanvas';
+import CanvasController from './Controllers/Maker/CanvasController';
+import init from './init';
 
 const main = () => {
     const initRet = init();
@@ -11,16 +9,18 @@ const main = () => {
         return;
     }
 
-    const {gl, colorBuffer, positionBuffer} = initRet;
-
-    const red = new Color(255, 0, 200)
+    const { gl, colorBuffer, positionBuffer } = initRet;
 
     const appCanvas = new AppCanvas(gl, positionBuffer, colorBuffer);
-    const triangle = new Triangle('tri-1', red, 50, 50, 20, 500, 200, 100);
-    appCanvas.addShape(triangle);
+    const canvasController = new CanvasController(appCanvas);
+    canvasController.start();
 
-    const line = new Line('line-1', red, 200, 100, 300, 100);
-    appCanvas.addShape(line);
+    // const red = new Color(255, 0, 200)
+    // const triangle = new Triangle('tri-1', red, 50, 50, 20, 500, 200, 100);
+    // appCanvas.addShape(triangle);
+
+    // const line = new Line('line-1', red, 200, 100, 300, 100);
+    // appCanvas.addShape(line);
 };
 
 main();
