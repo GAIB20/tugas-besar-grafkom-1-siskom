7923dc4eb2098692ddbd7646b7f09d8702af8a0a
Alisha Listya <73476678+alishalistyaa@users.noreply.github.com>
2024-04-03 09:33:49 +0700

    add: m3 utils

--- src/Controllers/Toolbar/Shape/RectangleToolbarController.ts
+++ src/Controllers/Toolbar/Shape/RectangleToolbarController.ts
@@ -17,30 +17,59 @@ export default class RectangleToolbarController extends ShapeToolbarController {
         this.rectangle = rectangle;
 
         // X Position
-        this.posXSlider = this.createSlider('Position X', rectangle.center.x,1,appCanvas.width);
+        this.posXSlider = this.createSlider('Position X', () => rectangle.center.x,1,appCanvas.width);
         this.registerSlider(this.posXSlider, (e) => {this.updatePosX(parseInt(this.posXSlider.value))})
 
         // Y Position
-        this.posYSlider = this.createSlider('Position Y', rectangle.center.y,1,appCanvas.width);
+        this.posYSlider = this.createSlider('Position Y', () => rectangle.center.y,1,appCanvas.width);
         this.registerSlider(this.posYSlider, (e) => {this.updatePosY(parseInt(this.posYSlider.value))})
 
     }
 
     private updatePosX(newPosX:number){
         const diff = newPosX - this.rectangle.center.x;
-        this.rectangle.center.x = newPosX;
         for (let i = 0; i < 4; i++) {
-            this.rectangle.pointList[i].x = this.rectangle.pointList[i].x + diff;
+            this.rectangle.pointList[i].x += diff;
         }
+        this.recalculateCenter();
         this.updateShape(this.rectangle);
     }
 
     private updatePosY(newPosY:number){
         const diff = newPosY - this.rectangle.center.y;
-        this.rectangle.center.y = newPosY;
         for (let i = 0; i < 4; i++) {
-            this.rectangle.pointList[i].y = this.rectangle.pointList[i].y + diff;
+            this.rectangle.pointList[i].y += diff;
         }
+        this.recalculateCenter();
         this.updateShape(this.rectangle);
+    }
+
+    updateVertex(idx: number, x: number, y: number): void {
+        const diffy = y - this.rectangle.pointList[idx].y;
+        const diffx = x - this.rectangle.pointList[idx].x;
+
+        for (let i = 0; i < 4; i++) {
+            if (i != idx) {
+                this.rectangle.pointList[i].y += diffy;
+                this.rectangle.pointList[i].x += diffx;
+            }
+        }
+
+        this.rectangle.pointList[idx].x = x;
+        this.rectangle.pointList[idx].y = y;
+
+        this.recalculateCenter();
+        this.updateShape(this.rectangle);
+    }
+    
+    private recalculateCenter(): void {
+        let sumX = 0, sumY = 0;
+        const vertices = this.rectangle.pointList;
+        vertices.forEach(vertex => {
+            sumX += vertex.x;
+            sumY += vertex.y;
+        });
+        this.rectangle.center.x = sumX / vertices.length;
+        this.rectangle.center.y = sumY / vertices.length;
     }
 }
